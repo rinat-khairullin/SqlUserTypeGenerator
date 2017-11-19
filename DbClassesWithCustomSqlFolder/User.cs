7bd7c710b4f4ed8f007d4d3d235a0425b2bbf500
rinat-khairullin <rinat.khairullin@gmail.com>
2017-11-19 11:53:36 +0300

    add second sample project

---
+++ DbClassesWithCustomSqlFolder/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SqlUserTypeGenerator;
+
+namespace DbClassesWithCustomSqlFolder
+{
+    [SqlUserType("user")]
+    public class User
+    {
+        public long PropLong { get; set; }
+        public long? PropLongNull { get; set; }
+        public string PropString { get; set; }
+        public bool PropBool { get; set; }
+        public bool? PropBoolNull { get; set; }
+        public DateTime PropDateTime { get; set; }
+        public DateTime? PropDateTimeNull { get; set; }
+        public decimal PropDecimal { get; set; }
+        public decimal? PropDecimalNull { get; set; }
+        public double PropDouble { get; set; }
+        public double? PropDoubleNull { get; set; }
+        public int PropInt { get; set; }
+        public int? PropIntNull { get; set; }
+        public Guid PropGuid { get; set; }
+        public Guid? PropGuidNull { get; set; }
+        public byte[] PropByteArray { get; set; }
+        public byte PropByte { get; set; }
+        public byte? PropByteNull { get; set; }
+    }
+}
