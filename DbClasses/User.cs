2382fbfa3bb6ef15e69bb67e22bed9ef06f9a9ee
rinat-khairullin <rinat.khairullin@gmail.com>
2018-02-12 23:39:20 +0300

    update readme

--- DbClasses/User.cs
+++ DbClasses/User.cs
@@ -47,4 +47,17 @@ namespace DbClasses
 		[SqlColumn(Length = 23)]
 		public string BaseProp { get; set; }
 	}
+
+	[SqlUserType(TypeName = "t_example")]
+	public class Example
+	{
+		[SqlColumn(Length = 42)]
+		public string NotNullString { get; set; }
+		[SqlColumn(Length = 10, Nullable = true)]
+		public string NullString { get; set; }
+		[SqlColumn(Length = SqlColumnAttribute.MaxLength)]
+		public string StringMax { get; set; }
+		[SqlColumn(Presicion = 7, Scale = 3)]
+		public decimal Decimal { get; set; }
+	}
 }
